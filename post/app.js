47a9348fa8a994460a480d983f39f999c33860b6
Keshav885 <kesava.paruchuri@aurotechcorp.com>
2017-07-18 17:19:40 -0400

    post

--- post/app.js
+++ post/app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app =express();
 var bodyParser =require("body-parser");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+
 
 
 var friends =["sam", "ham", "kim", "bil", "chill"];
