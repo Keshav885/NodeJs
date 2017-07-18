4a8bf4fa2c8892c5ce3d0c6a671952825c4847eb
Keshav885 <kesava.paruchuri@aurotechcorp.com>
2017-07-18 15:51:47 -0400

    friends post created

--- post/app.js
+++ post/app.js
@@ -5,6 +5,9 @@ app.get("/", function(req, res){
 	res.send("Home page");
 });
 
+app.get("/friends", function(){
+	res.send("This is friends page");
+});
 app.listen(3000, function(req, res){
 	console.log("Server is started form post");
 });
