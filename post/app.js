e6d11d71cabda76b2a94279a86d05fbba80fcdec
Keshav885 <kesava.paruchuri@aurotechcorp.com>
2017-07-18 16:24:52 -0400

    post req completed

--- post/app.js
+++ post/app.js
@@ -1,12 +1,22 @@
 var express = require("express");
 var app =express();
+var bodyParser =require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 
+
+var friends =["sam", "ham", "kim", "bil", "chill"];
 app.get("/", function(req, res){
 	res.send("Home page");
 });
 
-app.get("/friends", function(){
-	res.send("This is friends page");
+app.get("/friends", function(req, res){
+	res.render("friends.ejs", {friends:friends});
+});
+
+app.post("/addfriend", function(req, res){
+    var addnew = req.body.addnew;
+    friends.push(addnew);
+    res.redirect("/friends");
 });
 app.listen(3000, function(req, res){
 	console.log("Server is started form post");
