1a91aeac4d33d58ebaa8e6e538afc79135f79f23
Keshav885 <kesava.paruchuri@aurotechcorp.com>
2017-07-19 11:16:22 -0400

    url customized

--- movie_app/app.js
+++ movie_app/app.js
@@ -10,8 +10,10 @@ app.get("/", function(req, res){
 });
 
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=in&apikey=thewdb", function(error, response, body){
-    	var query = req.query.movieSearch;
+    var query = req.query.movieSearch;
+    var url = "http://www.omdbapi.com/?s=in&apikey=thewdb" + movieSearch;
+    request("url", function(error, response, body){
+    	
     	if(!error && response.statusCode ===  200){
     		var data =JSON.parse(body);
     		// res.send(results["Search"][0]["Title"]);
