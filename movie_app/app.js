739e7e1b8adbfc388b779f5387f3064c5eea9de7
Keshav885 <kesava.paruchuri@aurotechcorp.com>
2017-07-19 09:33:55 -0400

    server integrated to movie api

--- movie_app/app.js
+++ movie_app/app.js
@@ -5,6 +5,13 @@ var request =require("request");
 app.set("view.engine");
 
 
+app.get("/results", function(req, res){
+    request("http://www.omdbapi.com/?s=in&apikey=thewdb", function(error, response, body){
+    	if(!error && response.statusCode ===  200){
+    		res.send(body);
+    	}
+});
+
 app.listen(3000, function(req, res){
 	console.log("Server is started form post");
 });
